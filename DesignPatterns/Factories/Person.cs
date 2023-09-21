54a04fa703eeded1b15f502e7720222a13ef1a16
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-09-21 19:42:22 +0100

    create PersonFactory Exercice

---
+++ DesignPatterns/Factories/Person.cs
@@ -0,0 +1,45 @@
+﻿using DesignPatterns.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Factories
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonFactory
+    {
+        private int nextId = 0;
+
+        public Person CreatePerson(string name)
+        {
+            var person = new Person()
+            {
+                Id = nextId,
+                Name = name
+            };
+
+
+            nextId++;
+
+            return person;
+        }
+    }
+    public class PersonFactoryDemo : IDemo
+    {
+        public void DisplayResult()
+        {
+            var factory = new PersonFactory();
+            var person1 = factory.CreatePerson("Hamdi");
+            Console.WriteLine($"{person1.Id}: {person1.Name}");
+            var person2 = factory.CreatePerson("Nawfel");
+            Console.WriteLine($"{person2.Id}: {person2.Name}");
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,16 +1,17 @@
 ﻿using DesignPatterns.Factories;
 using DesignPatterns.Utils;
-using static DesignPatterns.Factories.HotDrinkMachine;
 
 var pointFactroyDemo = new PointFactroyDemo();
 var trackingThemeFactoryDemo = new TrackingThemeFactoryDemo();
 var hotDrinkMachineDemo = new HotDrinkMachineDemo();
+var personFactoryDemo = new PersonFactoryDemo();
 
 var demos = new List<IDemo>()
 {
     pointFactroyDemo,
     trackingThemeFactoryDemo,
-    hotDrinkMachineDemo
+    hotDrinkMachineDemo,
+    personFactoryDemo
 };
 
 

9e4ca7a40ed20ffb3055c4f7971230cef56fe318
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-09-23 20:18:25 +0100

    implement prototype pattern demo with copy constractor

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DesignPatterns.CharpTopics._1_Reflexion;
 using DesignPatterns.Factories;
+using DesignPatterns.Prototype;
 using DesignPatterns.Utils;
 using System.Collections.Generic;
 
@@ -8,22 +9,24 @@ var trackingThemeFactoryDemo = new TrackingThemeFactoryDemo();
 var hotDrinkMachineDemo = new HotDrinkMachineDemo();
 var personFactoryDemo = new PersonFactoryDemo();
 var creditCardDemo = new CreditCardDemo();
+var prototypeDemo = new PrototypeDemo();
 
 var demos = new List<IDisplayDemo>()
 {
     pointFactroyDemo,
-    trackingThemeFactoryDemo,
-    hotDrinkMachineDemo,
-    personFactoryDemo,
-    creditCardDemo
+    //trackingThemeFactoryDemo,
+    //hotDrinkMachineDemo,
+    //personFactoryDemo,
+    //creditCardDemo,
+    prototypeDemo
 };
 
 
-//var demo = new DisplayDemo(demos);
-//demo.Display();
+var demo = new DisplayDemo(demos);
+demo.Display();
 
 
-var executebledemo = new ReflectionDemo();
+//var executebledemo = new ReflectionDemo();
 
-var executableDemo = new ExecuteDemo(executebledemo);
-executableDemo.Execute();
+//var executableDemo = new ExecuteDemo(executebledemo);
+//executableDemo.Execute();
---
+++ DesignPatterns/Prototype/PrototypeDemo.cs
@@ -0,0 +1,67 @@
+﻿using DesignPatterns.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Prototype
+{
+    public class Person
+    {
+        public List<string> Names { get; set; }
+        public Address Address { get; set; }
+
+        public Person(List<string> names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Names)} : {string.Join(" ",Names)}, {nameof(Address)} : {Address}";
+        }
+    }
+    public class Address
+    {
+        public string ScreetName { get; set; }
+        public int HouseNumber { get; set; }
+        public Address(string screetName, int houseNumber)
+        {
+            ScreetName = screetName;
+            HouseNumber = houseNumber;
+        }
+
+        public Address(Address otherAddress)
+        {
+            ScreetName = otherAddress.ScreetName;
+            HouseNumber = otherAddress.HouseNumber;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(ScreetName)} : {ScreetName}, {nameof(HouseNumber)} : {HouseNumber}";
+        }
+    }
+
+    public class PrototypeDemo : IDisplayDemo
+    {
+        public void DisplayResult()
+        {
+            var nawfel = new Person(new List<string>() { "Nawfel", "Hamdi" }, new Address("Cite Khad", 123));
+            Console.WriteLine(nawfel);
+
+            var jan = new Person(nawfel);
+            jan.Names = new List<string> { "Jan", "jho" };
+            jan.Address.HouseNumber = 0;
+            Console.WriteLine(jan);
+            Console.WriteLine(nawfel);
+        }
+
+    }
+}

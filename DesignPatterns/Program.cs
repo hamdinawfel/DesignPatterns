6ff3dd6a5e4fdc6f86a68cdf5517a7d3678ab329
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-10-01 13:11:38 +0100

    implement domo for singleton pattern

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -23,6 +23,7 @@ var prototypeExecrice = new PrototypeExecrice();
 var singletionDemo = new SingletonDemo();
 var singletonMonostateDemo = new SingletonMonostateDemo();
 var perThreadSingletonDemo = new PerThreadSingletonDemo();
+var ambientContextDemo = new AmbientContextDemo();
 
 var demos = new List<IDisplayDemo>()
 {
@@ -40,6 +41,7 @@ var demos = new List<IDisplayDemo>()
     singletionDemo,
     singletonMonostateDemo,
     perThreadSingletonDemo,
+    ambientContextDemo,
 };
 
 
---
+++ DesignPatterns/Singleton/AmbientContextDemo.cs
@@ -0,0 +1,110 @@
+﻿
+using DesignPatterns.Utils;
+using System.Drawing;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public sealed class BuildingContext : IDisposable
+    {
+        public int WallHeight = 0;
+        private static Stack<BuildingContext> stack
+          = new Stack<BuildingContext>();
+
+        static BuildingContext()
+        {
+            stack.Push(new BuildingContext(0));
+        }
+
+        public BuildingContext(int wallHeight)
+        {
+            WallHeight = wallHeight;
+            stack.Push(this);
+        }
+
+        public static BuildingContext Current => stack.Peek();
+
+        public void Dispose()
+        {
+            if (stack.Count > 1)
+                stack.Pop();
+        }
+    }
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+           X = x;
+           Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(X)} : {X}, {nameof(Y)} : {Y}";
+        }
+    }
+
+    public class Wall
+    {
+        public Point Start, End;
+        public int Height;
+        public Wall(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+            Height = BuildingContext.Current.WallHeight;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}, " +
+                   $"{nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Building
+    {
+        public List<Wall> Walls = new List<Wall>();
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach(Wall wall in Walls)
+            {
+                sb.AppendLine(wall.ToString());
+            }
+            return sb.ToString();   
+        }
+    }
+
+
+    public class AmbientContextDemo : IDisplayDemo
+    {
+        public void DisplayResult()
+        {
+            var house = new Building();
+
+            // ground floor
+            //var e = 0;
+            house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0)));
+            house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)));
+
+            // first floor
+            //e = 3500;
+            using (new BuildingContext(3500))
+            {
+                house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0)));
+                house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)));
+            }
+
+            // back to ground again
+            house.Walls.Add(new Wall(new Point(5000, 0), new Point(5000, 4000)));
+
+            Console.WriteLine(house);
+
+        }
+    }
+}
